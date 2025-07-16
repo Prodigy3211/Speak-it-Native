d8b6f98217b43c2611e58fe0d8210748e8023e7e
prodigy3211 <37477839+Prodigy3211@users.noreply.github.com>
2025-07-16 14:47:15 -0400

    Update icons and images

--- speakIt-Mobile/lib/categoryImages.ts
+++ speakIt-Mobile/lib/categoryImages.ts
@@ -1,8 +1,8 @@
 export const CATEGORY_IMAGES = {
     'Prove Me Wrong': 'https://qdpammoeepwgapqyfrrh.supabase.co/storage/v1/object/public/speak-it-brand-assets/ProveMeWrong.png',
-    'Relationships': 'https://qdpammoeepwgapqyfrrh.supabase.co/storage/v1/object/public/speak-it-brand-assets/IMG_5281.png',
-    'War': 'https://qdpammoeepwgapqyfrrh.supabase.co/storage/v1/object/public/speak-it-brand-assets/Fred%20hampton%201969.jpg',
+    'Relationships': 'https://qdpammoeepwgapqyfrrh.supabase.co/storage/v1/object/public/speak-it-brand-assets//relationships.png',
+    'War': 'https://qdpammoeepwgapqyfrrh.supabase.co/storage/v1/object/public/speak-it-brand-assets//war.png',
     'Politics': 'https://qdpammoeepwgapqyfrrh.supabase.co/storage/v1/object/public/speak-it-brand-assets/Malcolm-x_colorized_photo.jpg',
-    'Philosophy': 'https://qdpammoeepwgapqyfrrh.supabase.co/storage/v1/object/public/speak-it-brand-assets/IMG_5281.png',
-    'Entertainment': 'https://qdpammoeepwgapqyfrrh.supabase.co/storage/v1/object/public/speak-it-brand-assets/IMG_5281.png'
+    'Philosophy': 'https://qdpammoeepwgapqyfrrh.supabase.co/storage/v1/object/public/speak-it-brand-assets//philosophy.png',
+    'Entertainment': 'https://qdpammoeepwgapqyfrrh.supabase.co/storage/v1/object/public/speak-it-brand-assets//entertainment.png'
 } as const; 
