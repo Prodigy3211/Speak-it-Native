a72ddbac30a01b3fd7bcc8511052c8e80a426593
prodigy3211 <37477839+Prodigy3211@users.noreply.github.com>
2026-02-20 14:35:15 -0500

    Add images to buttons on the home screen

--- speakIt-Mobile/app/Speak-It/Home.tsx
+++ speakIt-Mobile/app/Speak-It/Home.tsx
@@ -5,8 +5,9 @@
 //  Created by Amir Nasser on 7/9/25.
 //
 
+import { HOME_IMAGES } from '@/lib/homeImages';
 import { router } from 'expo-router';
-import { View, StatusBar,Text,Button } from 'react-native';
+import { Image, StatusBar,Text, View, TouchableOpacity, ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 
@@ -19,10 +20,79 @@ export default function Home() {
         translucent={false}
       />
 
-      <Text> Join the Discussion or stare at all the arguments you've posted.</Text>
-      <Button title='Trending' onPress={() => router.push('/Speak-It/Trending')} />
-
-      <Button title='My Claims' onPress={() => router.push('/Speak-It/MyClaims')} />
+      <View>
+      <Text style={{ 
+        fontSize: 24, 
+        fontWeight: 'bold', 
+        marginBottom: 16,
+        textAlign: 'center',
+        color: '#1a1a1a',
+        paddingHorizontal: 12,
+        shadowColor: '#1a1a1a',
+        shadowOffset: {
+          width: 0,
+          height: 4,
+        },
+        shadowOpacity: 0.1,
+        shadowRadius: 8,
+        elevation: 5,
+        backgroundColor: 'white',
+        borderRadius: 12,
+        padding: 12,
+        }}>
+           Join the Discussion or stare at all the arguments you've posted.
+           </Text>
+      </View>
+      <ScrollView
+      contentContainerStyle={{ 
+        paddingHorizontal: 20,
+        paddingTop: 16,
+      paddingBottom: 16,
+      gap: 20,
+     }}
+      >
+      <TouchableOpacity
+        onPress={() => router.push('/Speak-It/Trending')}
+        activeOpacity={0.8}
+        style = {{marginBottom: 16,
+          borderRadius: 12,
+          backgroundColor: '#f8f9fa',
+          shadowColor: '#1a1a1a',
+          shadowOffset: {
+            width: 0,
+            height: 4,
+          },
+          shadowOpacity: 0.1,
+          shadowRadius: 8,
+          elevation: 5,
+        }}
+        >
+        <Image source={{ uri: HOME_IMAGES.Trending }} style={{ width: '100%', height: 450}}
+        resizeMode='contain'
+        />        
+        </TouchableOpacity>
+        <TouchableOpacity
+        onPress={() => router.push('/Speak-It/MyClaims')}
+        activeOpacity={0.8}
+        style = {{
+          marginBottom: 16,
+          borderRadius: 12,
+          backgroundColor: '#f8f9fa',
+          shadowColor: '#1a1a1a',
+          shadowOffset: {
+            width: 0,
+            height: 4,
+          },
+          shadowOpacity: 0.1,
+          shadowRadius: 8,
+          elevation: 5,
+        }}
+        >
+        <Image source={{ uri: HOME_IMAGES.MyClaims }} style={{ width: '100%', height: 450}}
+        resizeMode='contain'
+        />        
+        </TouchableOpacity> 
+        </ScrollView>
       {/* <Trending /> */}
       {/* <MyClaims /> */}
     </SafeAreaView>
